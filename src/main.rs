c904d34e0e936f599b8ec9f2a12c320703d2f4c4
denjiry <k.denjiry@gmail.com>
2023-01-19 11:51:47 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,137 @@
-use smartcore::dataset::iris;
 use smartcore::linalg::naive::dense_matrix::DenseMatrix;
+use smartcore::linalg::BaseMatrix;
 use smartcore::linear::logistic_regression::LogisticRegression;
 use smartcore::model_selection::train_test_split;
-use smartcore::metrics::mean_absolute_error;
+
+// metrics
+use smartcore::metrics::mean_squared_error;
+use smartcore::metrics::accuracy;
+
+use std::convert::TryFrom;
+use std::fs::File;
+use std::path::Path;
+
+use polars::prelude::*;
+use polars::frame::DataFrame;
+use polars::prelude::Result as PolarResult;
+use polars::prelude::SerReader;
+
+
+//CSVファイルを読み込んでDataFrameを返す
+fn read_csv_with_schema<P: AsRef<Path>>(path: P) -> PolarResult<DataFrame> {
+    let schema = Schema::new(vec![
+        Field::new("species", DataType::Utf8),
+        Field::new("island", DataType::Utf8),
+        Field::new("culmen_length_mm", DataType::Float64),
+        Field::new("culmen_depth_mm", DataType::Float64),
+        Field::new("flipper_length_mm", DataType::Float64),
+        Field::new("body_mass_g", DataType::Float64),
+        Field::new("sex", DataType::Utf8)
+    ]);
+    let file = File::open(path).expect("Cannot open file.");
+    CsvReader::new(file)
+        .with_schema(Arc::new(schema))
+        .has_header(true)
+        .with_ignore_parser_errors(true) //エラーが出ても処理継続
+        .finish()
+}
+
+//featureとtargetに分割
+fn get_feature_target(df: &DataFrame) -> (PolarResult<DataFrame>, PolarResult<DataFrame>) {
+    let features = df.select(vec![
+        "culmen_length_mm",
+        "culmen_depth_mm",
+        "flipper_length_mm",
+        "body_mass_g",
+    ]);
+    let target = df.select("species");
+    (features, target)
+}
+
+/* 機能データフレームを smartcore で読める DenseMatrix に変換 */
+pub fn convert_features_to_matrix(df: &DataFrame) -> Result<DenseMatrix<f64>> {
+    let nrows = df.height();
+    let ncols = df.width();
+
+    let features_res = df.to_ndarray::<Float64Type>().unwrap();
+    let mut xmatrix: DenseMatrix<f64> = BaseMatrix::zeros(nrows, ncols);
+    let mut col: u32 = 0;
+    let mut row: u32 = 0;
+
+    for val in features_res.iter() {
+        let m_row = usize::try_from(row).unwrap();
+        let m_col = usize::try_from(col).unwrap();
+        xmatrix.set(m_row, m_col, *val);
+        // check what we have to update
+        if m_col == ncols - 1 {
+            row += 1;
+            col = 0;
+        } else {
+            col += 1;
+        }
+    }
+
+    Ok(xmatrix)
+}
+
+//speciesのLabelエンコーディング用function
+fn str_to_num(str_val: &Series) -> Series {
+    str_val
+        .utf8()
+        .unwrap()
+        .into_iter()
+        .map(|opt_name: Option<&str>| {
+            opt_name.map(|name: &str| {
+                match name {
+                    "Adelie" => 1,
+                    "Chinstrap" => 2,
+                    "Gentoo" => 3,
+                    _ => panic!("Problem species str to num"),
+                }
+            })
+        })
+        .collect::<UInt32Chunked>()
+        .into_series()
+}
 
 fn main() {
-    let iris_data = iris::load_dataset();
-    let x = DenseMatrix::from_array(
-        iris_data.num_samples,
-        iris_data.num_features,
-        &iris_data.data,
-    );
-    let y = iris_data.target;
 
-    let (x_train, x_test, y_train, y_test) = train_test_split(&x, &y, 0.2, true);
+    //csv読み込み
+    let csv = "penguins_size.csv";
+    let df: DataFrame = read_csv_with_schema(&csv).unwrap();
 
-    let reg = LogisticRegression::fit(
-        &x_train,
-        &y_train,
-        Default::default(),
-    ).unwrap();
+    //不正データdrop
+    let df2 = df.drop_nulls(None).unwrap();
 
-    let result = reg.predict(&x_test).unwrap();
-    assert!(mean_absolute_error(&y_test, &result) < 0.2);
+    //featrures/target分割
+    let (feature, target) = get_feature_target(&df2);
+
+    //features変換
+    let xmatrix = convert_features_to_matrix(&feature.unwrap());
+
+    //speciesのLabelエンコーディング
+    let target_array = target
+        .unwrap()
+        .apply("species", str_to_num)
+        .unwrap()
+        .to_ndarray::<Float64Type>()
+        .unwrap();
+
+    // create a vec type and populate with y values
+    let mut y: Vec<f64> = Vec::new();
+    for val in target_array.iter() {
+        y.push(*val);
+    }
+
+    //データ分割
+    let (x_train, x_test, y_train, y_test) = train_test_split(&xmatrix.unwrap(), &y, 0.3, true);
+
+    //学習
+    let reg = LogisticRegression::fit(&x_train, &y_train, Default::default()).unwrap();
+
+    //予測
+    let preds = reg.predict(&x_test).unwrap();
+    let mse = mean_squared_error(&y_test, &preds);
+    println!("MSE: {}", mse);
+    println!("accuracy : {}", accuracy(&y_test, &preds));
 }
